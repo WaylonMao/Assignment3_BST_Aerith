f0127fbcdda33e39889c0579b41f4323e4c2d71e
WaylonM <weilong.mao@edu.sait.ca>
2022-11-26 18:06:37 -0700

    Modified some part, now can run. TODO: repository file reading and writing.

--- Assignment3_BST_Aerith/src/application/WordTracker.java
+++ Assignment3_BST_Aerith/src/application/WordTracker.java
@@ -2,22 +2,28 @@ package application;
 
 import exceptions.TreeException;
 import utilities.BSTree;
-import utilities.BSTreeNode;
 import utilities.Iterator;
-import utilities.WordManager;
+import modules.Word;
 
 import java.io.*;
 import java.util.*;
+
+/**
+ * Class description: test arguments: "Assignment3_BST_Aerith/res/textfile.txt" -po
+ *
+ * @author
+ */
 
 public class WordTracker {
     private static String fileOutputName;
     private static String fileInputName;
     private static String userOption;
+    static final File REPOSITORY_FILE = new File("Assignment3_BST_Aerith/ser/repository.ser");
+
     public static void main(String[] args) {
         //create new binary file to store words if it does not exist
-        File repositoryFile = new File("ser/repository.ser");
-        BSTree<WordManager> wordTracker = new BSTree<>();
-        BSTree<WordManager> repository = new BSTree<>();
+
+        BSTree<Word> wordTree = new BSTree<>();
 
 
         // use scanner to read txt file
@@ -26,33 +32,34 @@ public class WordTracker {
 
             Scanner sc = new Scanner(new File(fileInputName));
             int lineCount = 0; // count the line number
+/**
+ // read from repository binary file,if it doesn't exist, create a new one
+ try {
+ ObjectInputStream ois = new ObjectInputStream(new FileInputStream(REPOSITORY_FILE));
+ ois.close();
 
-            // read from repository binary file,if it doesn't exist, create a new one
-            try {
-                ObjectInputStream ois = new ObjectInputStream(new FileInputStream(repositoryFile));
-                ois.close();
+ } catch (FileNotFoundException ex) {// when the repository file doesn't exist, create a new one
+ wordTree = new BSTree<>();
+ ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(REPOSITORY_FILE));
+ oos.writeObject(wordTree); // write the new tree to the repository file
+ oos.close();
 
-            } catch (FileNotFoundException ex) {// when the repository file doesn't exist, create a new one
-                wordTracker = new BSTree<>();
-                ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(repositoryFile));
-                oos.writeObject(wordTracker); // write the new tree to the repository file
-                oos.close();
+ }
+ ObjectInputStream ois = new ObjectInputStream(new FileInputStream(REPOSITORY_FILE));
 
-            }
-            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(repositoryFile));
-            repository = (BSTree<WordManager>) ois.readObject();
-            wordTracker = repository;
-            ois.close();
+ wordTree = (BSTree<Word>) ois.readObject();
+ ois.close();
 
-            // when user choose the output option, output the repository file
-            if(fileOutputName != null){
-//                ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileOutputName));
-//                oos.writeObject(repository);
-//                oos.close();
-//            }
-                PrintStream out = new PrintStream(fileOutputName);
-                System.setOut(out);
-            }
+ // when user choose the output option, output the repository file
+ if (fileOutputName != null) {
+ //                ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileOutputName));
+ //                oos.writeObject(repository);
+ //                oos.close();
+ //            }
+ PrintStream out = new PrintStream(fileOutputName);
+ System.setOut(out);
+ }
+ **/
             // find the word in the input file, if it is a new word, add it to the node of binary tree. Otherwise, add the line number to the node
             while (sc.hasNextLine()) {
                 lineCount++;
@@ -61,68 +68,69 @@ public class WordTracker {
                     if (!Character.isLetter(line.charAt(i)) && !Character.isWhitespace(line.charAt(i))) {
                         line = line.replaceAll("\\s", " ");
                         line = line.replace(line.charAt(i) + "", " ");
-
                     }
                 }
                 for (String word : line.split("\\s")) {
-                    WordManager addWord = new WordManager(lineCount, fileInputName, word);
-                    if (wordTracker.search(addWord) == null) {
-                        wordTracker.add(addWord);
+                    Word addWord = new Word(word, lineCount, fileInputName);
+                    if (wordTree.isEmpty() || wordTree.search(addWord) == null) {
+                        wordTree.add(addWord);
                         System.out.println(addWord.getWord() + " is added");
-                    }else{
-                        wordTracker.search(addWord).addRecord(addWord);
-                        System.out.println(addWord.getWord()+ " :is already stored in the repository");
+                    } else {
+                        wordTree.search(addWord).getElement().addCount(lineCount, fileInputName);
+                        System.out.println(addWord.getWord() + " :is already stored in the repository");
                     }
                 }
             }
             // write the binary tree to the repository file
-            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(repositoryFile));
-            oos.writeObject(wordTracker);
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(REPOSITORY_FILE));
+            oos.writeObject(wordTree);
             oos.close();
 
             // Iterate through the tree and print out the words and their records
-            Iterator<WordManager> iterator =  wordTracker.inorderIterator();
+            Iterator<Word> iterator = wordTree.inorderIterator();
             while (iterator.hasNext()) {
-                WordManager word = iterator.next();
-                System.out.println("Display word: "+ word.getWord());
-
-                BSTreeNode<WordManager> node = wordTracker.search(word);
-                //Iterator<WordManager> it = (Iterator<WordManager>) node.getList().iterator();
-
-                if(userOption.equals("f")) {
-                    System.out.println("A word: " + word.getWord() + " is found in " + word.getFileName() + " files");
-                }else if(userOption.equals("l")){
-                    System.out.println("A word: "+ word.getWord() + " is found in " + word.getFileName() + " files at line number: " + word.getLinePosition());
-                }else if(userOption.equals("o")){
-                    System.out.println("A word: "+ word.getWord() + " on line number: " + word.getLinePosition() + " have frequency of occurrence: "+ node.getSize());
+                Word word = iterator.next();
+                System.out.println("Display word: " + word.getWord());
+                List<String> fileNames = word.getFileNames();
+                List<Integer> lineNumbers = word.getLineNumbers();
+                if (userOption.equals("f")) {
+                    for (int i = 0; i < fileNames.size(); i++) {
+                        System.out.println("File name: " + fileNames.get(i));
+                    }
+                } else if (userOption.equals("l")) {
+                    for (int i = 0; i < fileNames.size(); i++) {
+                        System.out.println("File name: " + fileNames.get(i) + " Line number: " + lineNumbers.get(i));
+                    }
+                } else if (userOption.equals("o")) {
+                    for (int i = 0; i < fileNames.size(); i++) {
+                        System.out.println("File name: " + fileNames.get(i) + " Line number: " + lineNumbers.get(i));
+                    }
+                    System.out.println("Occurrence: " + word.getCount());
                 }
             }
-
-
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException | IOException | TreeException e) {
+        } catch (IOException | TreeException e) {
             throw new RuntimeException(e);
         }
     }
 
-        private void parseArgs(String[] args){
-        if(args.length == 0|| args.length > 3 || args.length <2 ) System.out.println("There were errors on the arguments");
-        else{
-            for(int i = 0; i < args.length; i++){
+    private void parseArgs(String[] args) {
+        if (args.length == 0 || args.length > 3 || args.length < 2)
+            System.out.println("There were errors on the arguments");
+        else {
+            for (int i = 0; i < args.length; i++) {
                 // when the file start with -f, the rest of the string is the file name
-                if(args[i].toLowerCase().substring(0,2).equals("-f")) {
+                if (args[i].toLowerCase().substring(0, 2).equals("-f")) {
                     fileOutputName = args[i].substring(2); // get the output file name
                     System.out.println("Output file name is: " + fileOutputName);
-                // when the file start with -p, the rest of the string is the option from user
-                } else if (args[i].toLowerCase().substring(0,2).equals("-p") && args[i].length()  == 3){
+                    // when the file start with -p, the rest of the string is the option from user
+                } else if (args[i].toLowerCase().substring(0, 2).equals("-p") && args[i].length() == 3) {
                     userOption = args[i].substring(2); // get the input file name
                     System.out.println("User option is: " + userOption + " ");
-                } else{
+                } else {
                     fileInputName = args[i];
                     System.out.println("Input file name is: " + fileInputName);
                 }
-        }
+            }
 
         }
 

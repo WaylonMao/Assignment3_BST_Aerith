cc10396eb6d69beb0ed97724e046c6b17ffb28d5
philippenghu <ph174175275@gmail.com>
2022-11-28 01:04:30 -0700

    Add test for BSTree node and correct an error

--- Assignment3_BST_Aerith/src/utilities/BSTreeNode.java
+++ Assignment3_BST_Aerith/src/utilities/BSTreeNode.java
@@ -4,92 +4,92 @@ import java.io.Serializable;
 
 public class BSTreeNode<E extends Comparable<? super E>> implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private E element;
+	private E element;
 
-    private BSTreeNode<E> left, right;
+	private BSTreeNode<E> left, right;
 
-    public BSTreeNode() {
-        this(null);
-    }
+	public BSTreeNode() {
+		this(null);
+	}
 
-    public BSTreeNode(E element) {
-        this(element, null, null);
-    }
+	public BSTreeNode(E element) {
+		this(element, null, null);
+	}
 
-    public BSTreeNode(E element, BSTreeNode<E> left, BSTreeNode<E> right) {
-        this.element = element;
-        this.left = left;
-        this.right = right;
-    }
+	public BSTreeNode(E element, BSTreeNode<E> left, BSTreeNode<E> right) {
+		this.element = element;
+		this.left = left;
+		this.right = right;
+	}
 
-    public boolean add(BSTreeNode<E> newNode) {
-        if (newNode == null) {
-            return false;
-        }
+	public boolean add(BSTreeNode<E> newNode) {
+		if (newNode == null) {
+			return false;
+		}
 
-        if (newNode.getElement().compareTo(this.element) < 0) {
-            if (left == null) {
-                left = newNode;
-            } else {
-                left.add(newNode);
-            }
-        } else if (newNode.getElement().compareTo(this.element) > 0) {
-            if (right == null) {
-                right = newNode;
-            } else {
-                right.add(newNode);
-            }
-        }
-        return true;
-    }
+		if (newNode.getElement().compareTo(this.element) <= 0) {
+			if (left == null) {
+				left = newNode;
+			} else {
+				left.add(newNode);
+			}
+		} else if (newNode.getElement().compareTo(this.element) > 0) {
+			if (right == null) {
+				right = newNode;
+			} else {
+				right.add(newNode);
+			}
+		}
+		return true;
+	}
 
-    public E getElement() {
-        return element;
-    }
+	public E getElement() {
+		return element;
+	}
 
-    public void setElement(E element) {
-        this.element = element;
-    }
+	public void setElement(E element) {
+		this.element = element;
+	}
 
-    public BSTreeNode<E> getLeft() {
-        return left;
-    }
+	public BSTreeNode<E> getLeft() {
+		return left;
+	}
 
-    public void setLeft(BSTreeNode<E> left) {
-        this.left = left;
-    }
+	public void setLeft(BSTreeNode<E> left) {
+		this.left = left;
+	}
 
-    public BSTreeNode<E> getRight() {
-        return right;
-    }
+	public BSTreeNode<E> getRight() {
+		return right;
+	}
 
-    public void setRight(BSTreeNode<E> right) {
-        this.right = right;
-    }
+	public void setRight(BSTreeNode<E> right) {
+		this.right = right;
+	}
 
-    public boolean hasLeft() {
-        return left != null;
-    }
+	public boolean hasLeft() {
+		return left != null;
+	}
 
-    public boolean hasRight() {
-        return right != null;
-    }
+	public boolean hasRight() {
+		return right != null;
+	}
 
-    public boolean isLeaf() {
-        return (left == null) && (right == null);
-    }
+	public boolean isLeaf() {
+		return (left == null) && (right == null);
+	}
 
-    public int getHeight() {
-        int leftHeight = 0;
-        int rightHeight = 0;
-        if (this.left != null) {
-            leftHeight = this.left.getHeight();
-        }
-        if (this.right != null) {
-            rightHeight = this.right.getHeight();
-        }
-        return 1 + Math.max(leftHeight, rightHeight);
-    }
+	public int getHeight() {
+		int leftHeight = 0;
+		int rightHeight = 0;
+		if (this.left != null) {
+			leftHeight = this.left.getHeight();
+		}
+		if (this.right != null) {
+			rightHeight = this.right.getHeight();
+		}
+		return 1 + Math.max(leftHeight, rightHeight);
+	}
 }
